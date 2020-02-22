eca313172624f3fe94316728f9a41eb386ac5b70
taka1995 <38433955+taka1995@users.noreply.github.com>
2020-02-22 14:51:18 +0900

    Update app.py

--- app/app.py
+++ app/app.py
@@ -10,6 +10,12 @@ def index():
     menhera = ["返信まだ？","返信まだ?","返信まだ？","浮気？笑"]
     return render_template("index.html",name=name, menhera=menhera)
 
+@app.route("/index",methods=["post"])
+def post():
+    name = request.form["name"]
+    menhera = ["返信まだ？","返信まだ?","返信まだ？","浮気？笑"]
+    return render_template("index.html",name=name, menhera=menhera)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
