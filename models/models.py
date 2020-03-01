7858552ea8099ba0e61dc6ec2356f80a73871870
taka1995 <38433955+taka1995@users.noreply.github.com>
2020-03-01 20:53:51 +0900

    Update models.py

--- models/models.py
+++ models/models.py
@@ -3,8 +3,8 @@ from models.database import Base
 from datetime import datetime
 
 
-class OnegaiContent(Base):
-    __tablename__ = 'onegaicontents'
+class yamiyamiContent(Base):
+    __tablename__ = 'yamiyamicontents'
     id = Column(Integer, primary_key=True)
     title = Column(String(128), unique=True)
     body = Column(Text)
