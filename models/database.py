f9d247440cc29230aa133753f0983c2bede6a74c
taka1995 <38433955+taka1995@users.noreply.github.com>
2020-03-18 20:59:06 +0900

    Update database.py

--- models/database.py
+++ models/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
-databese_file = os.path.join(os.path.abspath(os.path.dirname(__file__)),  'yamiyami.db')
+databese_file = os.path.join(os.path.abspath(os.path.dirname(__file__)),  'onegai.db')
 engine = create_engine('sqlite:///' + databese_file, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,autoflush=False,bind=engine))
 Base = declarative_base()
@@ -13,4 +13,3 @@ Base.query = db_session.query_property()
 def init_db():
     import models.models
     Base.metadata.create_all(bind=engine)
-    
